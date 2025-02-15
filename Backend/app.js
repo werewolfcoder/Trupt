2a4577562f80dde13f02c830af71f6448d354073
werewolfcoder <157696808+werewolfcoder@users.noreply.github.com>
2025-02-15 11:28:04 +0530

    test1

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+
+
+
+app.get('/',(req,res)=>{
+    res.send("kallu")
+})
+
+
+module.exports = app;
