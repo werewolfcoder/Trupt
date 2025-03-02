24322f364bafcba7b9a6aa804f8ca0b04412a97b
werewolfcoder <157696808+werewolfcoder@users.noreply.github.com>
2025-03-02 21:04:43 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     define: {
-    __DEFINES__: {}
+    __DEFINES__: {},
+       __HMR_CONFIG_NAME__:{}
   }
   // No define section needed with our fix
 })
